d1ee153705b86ffd0e7824e3755933c1e0d144e8
Chris Sefkin <c.sefkin@gmail.com>
2015-05-08 13:23:09 -0700

    provide working front end

--- app.rb
+++ app.rb
@@ -12,3 +12,77 @@ get('/') do
   @stylists = Stylist.all()
   erb(:index)
 end
+
+get('/clients') do
+  @clients = Client.all()
+  erb(:clients)
+end
+
+post('/clients') do
+  name = params.fetch("name")
+  client = Client.new({:name => name, :id => nil})
+  client.save()
+  @clients = Client.all()
+  erb(:clients)
+end
+
+get('/stylists') do
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+post('/stylists') do
+  name = params.fetch("name")
+  stylist = Stylist.new({:name => name, :id => nil})
+  stylist.save()
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+get('/clients/:id') do
+  id = params.fetch("id").to_i()
+  @client = Client.find(id)
+  @stylists = Stylist.all()
+  erb(:client_info)
+end
+
+# patch('/clients/:id') do
+#   client_id = params.fetch("id").to_i()
+#   @client = Client.find(client_id)
+#   stylist_ids = params.fetch("stylist_ids", [])
+#   @client.update({:stylist_ids => stylist_ids})
+#   @stylists = Stylist.all()
+#   erb(:client_info)
+# end
+
+delete('/clients/:id') do
+  client_id = params.fetch("id").to_i()
+  client = Client.find(client_id)
+  client.delete()
+  @clients = Client.all()
+  erb(:clients)
+end
+
+get('/stylists/:id') do
+  id = params.fetch("id").to_i()
+  @stylist = Stylist.find(id)
+  @clients = Client.all()
+  erb(:stylist_info)
+end
+
+patch('/stylists/:id') do
+  stylist_id = params.fetch("id").to_i()
+  @stylist = Stylist.find(stylist_id)
+  client_ids = params.fetch("client_ids", [])
+  @stylist.update({:client_ids => client_ids})
+  @clients = Client.all()
+  erb(:stylist_info)
+end
+
+delete('/stylists/:id') do
+  stylist_id = params.fetch("id").to_i()
+  stylist = Stylist.find(stylist_id)
+  stylist.delete()
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
